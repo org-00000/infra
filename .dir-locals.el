07adbdc01f7ad07ccac6dd8065621c8cdcd058b2
Pierre-Henry Frohring <phf@pc1.home>
2026-05-08 15:42:37 +0000

    Backlog → Tasks → DONE Update init.el for Elixir

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,10 @@
 ;;; .dir-locals.el -*- lexical-binding: t; no-byte-compile: t -*-
-
 ((org-mode
-  (fill-column . 85)
-  (org-confirm-babel-evaluate . nil)
-  (eval . (add-hook 'before-save-hook #'whitespace-cleanup nil t))
-  (eval . (add-hook 'after-save-hook #'lar-reload nil t))))
+   (fill-column . 85)
+   (org-confirm-babel-evaluate . nil)
+   (eval . (add-hook 'before-save-hook #'whitespace-cleanup nil t))
+   (eval . (add-hook 'after-save-hook #'lar-reload nil t)))
+  (elixir-mode
+    (eval . (setq-local eglot-workspace-configuration
+              `(:elixirLS (:projectDir ,(or (getenv "BACKEND")
+                                          (error "BACKEND env var is not set"))))))))
